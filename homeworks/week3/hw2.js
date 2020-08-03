2070e661911c2996f9f008fed2e16d72178d1615
Teiekinei <teiekinei.rofye@gmail.com>
2020-08-04 01:04:13 +0800

    hw6 update hw1,3,4

---
+++ homeworks/week3/hw2.js
@@ -0,0 +1,25 @@
+// const readline = require('readline');
+
+// const lines = [];
+// const rl = readline.createInterface({
+//   input: process.stdin,
+// });
+
+// rl.on('line', (line) => {
+//   lines.push(line);
+// });
+
+// function solve(n) {
+//   let temp = lines[0].split(' ')
+//   let n = Number(temp[0])
+//   let m = Number(temp[1])
+//   for(let i = n; i <= m; i += 1) {
+//     if (isNarcissistic(i)) {
+//       console.log(i)
+//     }
+//   }
+// }
+
+// rl.on('close', () => {
+//   solve(lines);
+// });
