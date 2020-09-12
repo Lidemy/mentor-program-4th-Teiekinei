2918bc84d0ae7b301e333740633882304d3cd5ba
Alina <alina@canjune.com.tw>
2020-09-13 02:56:36 +0800

    hw2 OK

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,25 +1,28 @@
-// const readline = require('readline');
+const readline = require('readline');
 
-// const lines = [];
-// const rl = readline.createInterface({
-//   input: process.stdin,
-// });
+const lines = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+});
 
-// rl.on('line', (line) => {
-//   lines.push(line);
-// });
+rl.on('line', (line) => {
+  lines.push(line);
+});
 
-// function solve(n) {
-//   let temp = lines[0].split(' ')
-//   let n = Number(temp[0])
-//   let m = Number(temp[1])
-//   for(let i = n; i <= m; i += 1) {
-//     if (isNarcissistic(i)) {
-//       console.log(i)
-//     }
-//   }
-// }
+function solve(input) {
+  const temp = input[0].split(' ');
+  const n = Number(temp[0]);
+  const m = Number(temp[1]);
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371,
+    407, 1634, 8208, 9474, 54748, 92727, 93084, 548834];
+  for (let i = 0; i <= arr.length; i += 1) {
+    const result = arr[i];
+    if (result >= n && result <= m) {
+      console.log(result);
+    }
+  }
+}
 
-// rl.on('close', () => {
-//   solve(lines);
-// });
+rl.on('close', () => {
+  solve(lines);
+});
